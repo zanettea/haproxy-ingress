23fcbd6748ef0411b4c42fb09623beea087fbd51
Arrigo Zanette <arrigo.zanette@corvina.io>
2026-07-23 15:33:04 +0200

    allow passing arguments

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-docker build -f builder/Dockerfile -t eu.gcr.io/corvina-exorint/haproxy-ingress-controller:v1 .
+GIT_COMMIT=git-$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
+GIT_REPO=$(git config --get remote.origin.url 2>/dev/null || echo "unknown")
+
+docker build \
+  --build-arg GIT_COMMIT="$GIT_COMMIT" \
+  --build-arg GIT_REPO="$GIT_REPO" \
+  -f builder/Dockerfile -t eu.gcr.io/corvina-exorint/haproxy-ingress-controller:v1 .
 
